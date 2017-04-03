1fa9b418ec71bd84874c2788f2f990cf20d164af
Dmitry Sheiko <dmitry@crytek.com>
2017-04-03 16:53:55 +0200

    swap via a script, not via NW.js

---
+++ scripts/swap.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+for i in "$@"
+do
+case $i in
+  --app-path=*)
+    APP_PATH="${i#*=}"
+    shift
+    ;;
+  --bak-path=*)
+    BAK_PATH="${i#*=}"
+    shift
+    ;;
+  --update-path=*)
+    UPDATE_PATH="${i#*=}"
+    shift
+    ;;
+  --runner=*)
+    RUNNER="${i#*=}"
+    shift
+    ;;
+    *)
+            # unknown option
+    ;;
+esac
+done
+
+rm -rf ${BAK_PATH}
+cp -rf ${APP_PATH} ${BAK_PATH}
+cp -rf ${UPDATE_PATH} ${APP_PATH}
+$($RUNNER)
