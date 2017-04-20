e2fc3d2354824062cb461c95d40e0d21c9f2209f
Dmitry Sheiko <dmitry@crytek.com>
2017-04-20 12:37:00 +0200

    getting ready for Windows

--- Lib/Swap/Win.js
+++ Lib/Swap/Win.js
@@ -7,67 +7,39 @@ class SwapWin extends SwapAbstract{
   constructor( options ){
     super( options );
   }
-
+  // @see http://steve-jansen.github.io/guides/windows-batch-scripting/part-2-variables.html
   getScriptContent(){
     return `@echo off
-setlocal enableDelayedExpansion
+SET execDir=%~1
+SET updateDir=%~2
+SET backupDir=%~3
+SET runner=%~4
+SET verbose=%~5
 
-set "options=-app-path:"" -bak-path:"" -update-path:"" -runner:"" -nope:"
+rmdir %backupDir% /s /q
+xcopy %execDir% %backupDir% /e /i /h /c /y
+xcopy %updateDir% %execDir% /e /i /h /c /y
 
-for %%O in (%options%) do for /f "tokens=1,* delims=:" %%A in ("%%O") do set "%%A=%%~B"
-:loop
-if not "%~3"=="" (
-  set "test=!options:*%~3:=! "
-  if "!test!"=="!options! " (
-      echo Error: Invalid option %~3
-  ) else if "!test:~0,1!"==" " (
-      set "%~3=1"
-  ) else (
-      setlocal disableDelayedExpansion
-      set "val=%~4"
-      call :escapeVal
-      setlocal enableDelayedExpansion
-      for /f delims^=^ eol^= %%A in ("!val!") do endlocal&endlocal&set "%~3=%%A" !
-      shift /3
-  )
-  shift /3
-  goto :loop
-)
-goto :endArgs
-:escapeVal
-set "val=%val:^=^^%"
-set "val=%val:!=^!%"
-exit /b
-:endArgs
-
-set -
-
-rem rmdir !-bak-path! /s /q
-rem xcopy !-app-path! !-bak-path! /e /i /h
-rem xcopy !-update-path! !-app-path! /e /i /h
-
-!-runner!
+%runner%
 `;
   }
 
   extractScript( homeDir )
   {
-    // @see http://steve-jansen.github.io/guides/windows-batch-scripting/part-2-variables.html
     const content = this.getSwapScriptContent(),
           scriptPath = join( homeDir, "swap.bat" );
     fs.writeFileSync( scriptPath, content, "utf8" );
     this.scriptPath = scriptPath;
   }
 
-/**
+  /**
    * Get args for swap script
    * @returns {Array}
    */
   getArgs()
   {
-      const { execDir, updateDir, executable, backupDir, logDir  } = this.options;
-      return [ `-app-path=${execDir}`, `-update-path=${updateDir}`, `-runner=${executable}`,
-            `-bak-path=${backupDir}` ];
+      const { execDir, updateDir, executable, backupDir, logDir } = this.options;
+      return [ execDir, updateDir, backupDir, executable, `false` ];
   }
 
 }
